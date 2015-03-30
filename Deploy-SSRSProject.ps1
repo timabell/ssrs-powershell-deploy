d2f2e8717b771b1dc265430d49f2f95e074dcb8a
Tim Abell <tim@timwise.co.uk>
2015-03-30 18:31:56 +0100

    refactor - extract method for report handling
    still too big, but it's a start

--- Deploy-SSRSProject.ps1
+++ Deploy-SSRSProject.ps1
@@ -178,6 +178,76 @@ function New-SSRSDataSet (
 	return $Results
 }
 
+function New-SSRSReport (
+	$Proxy,
+	[string]$RdlPath
+) {
+	[xml]$Definition = Get-Content -Path $RdlPath
+	$NsMgr = New-XmlNamespaceManager $Definition d
+
+	$RawDefinition = Get-Content -Encoding Byte -Path $RdlPath
+
+	$Name = $_.Name -replace '\.rdl$',''
+
+	$DescProp = New-Object -TypeName SSRS.ReportingService2010.Property
+	$DescProp.Name = 'Description'
+	$DescProp.Value = ''
+	$HiddenProp = New-Object -TypeName SSRS.ReportingService2010.Property
+	$HiddenProp.Name = 'Hidden'
+	$HiddenProp.Value = 'false'
+	$Properties = @($DescProp, $HiddenProp)
+
+	$Xpath = 'd:Report/d:Description'
+	$DescriptionNode = $Definition.SelectSingleNode($Xpath, $NsMgr)
+
+	if($DescriptionNode)
+	{
+		$DescProp.Value = $DescriptionNode.Value
+	}
+
+	if($Name.StartsWith('_'))
+	{
+		$HiddenProp.Value = 'true'
+	}
+
+	Write-Verbose "Creating report $Name"
+	$warnings = $null
+	$Results = $Proxy.CreateCatalogItem("Report", $Name, $Folder, $true, $RawDefinition, $Properties, [ref]$warnings)
+
+	$Xpath = 'd:Report/d:DataSources/d:DataSource/d:DataSourceReference/..'
+	$DataSources = $Definition.SelectNodes($Xpath, $NsMgr) |
+		ForEach-Object {
+			$DataSourcePath = $DataSourcePaths[$_.DataSourceReference]
+			if (-not $DataSourcePath) {
+				throw "Invalid data source reference '$($_.DataSourceReference)' in $RdlPath"
+			}
+			$Reference = New-Object -TypeName SSRS.ReportingService2010.DataSourceReference
+			$Reference.Reference = $DataSourcePath
+			$DataSource = New-Object -TypeName SSRS.ReportingService2010.DataSource
+			$DataSource.Item = $Reference
+			$DataSource.Name = $_.Name
+			Write-Verbose $DataSource
+		}
+	if ($DataSources) {
+		$Proxy.SetItemDataSources($Folder + '/' + $Name, $DataSources)
+	}
+
+	$Xpath = 'd:Report/d:DataSets/d:DataSet/d:SharedDataSet/d:SharedDataSetReference/../..'
+	$References = $Definition.SelectNodes($Xpath, $NsMgr) |
+		ForEach-Object {
+			$DataSetPath = $DataSetPaths[$_.SharedDataSet.SharedDataSetReference]
+			if ($DataSetPath) {
+				$Reference = New-Object -TypeName SSRS.ReportingService2010.ItemReference
+				$Reference.Reference = $DataSetPath
+				$Reference.Name = $_.Name
+				Write-Verbose $Reference
+			}
+		}
+	if ($References) {
+		$Proxy.SetItemReferences($Folder + '/' + $Name, $References)
+	}
+}
+
 $script:ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 
@@ -259,68 +329,5 @@ $Project.SelectNodes('Project/Reports/ResourceProjectItem') |
 $Project.SelectNodes('Project/Reports/ProjectItem') |
 	ForEach-Object {
 		$RdlPath = $ProjectRoot | Join-Path -ChildPath $_.FullPath
-		[xml]$Definition = Get-Content -Path $RdlPath
-		$NsMgr = New-XmlNamespaceManager $Definition d
-
-		$RawDefinition = Get-Content -Encoding Byte -Path $RdlPath
-
-		$Name = $_.Name -replace '\.rdl$',''
-
-		$DescProp = New-Object -TypeName SSRS.ReportingService2010.Property
-		$DescProp.Name = 'Description'
-		$DescProp.Value = ''
-		$HiddenProp = New-Object -TypeName SSRS.ReportingService2010.Property
-		$HiddenProp.Name = 'Hidden'
-		$HiddenProp.Value = 'false'
-		$Properties = @($DescProp, $HiddenProp)
-
-		$Xpath = 'd:Report/d:Description'
-		$DescriptionNode = $Definition.SelectSingleNode($Xpath, $NsMgr)
-
-		if($DescriptionNode)
-		{
-			$DescProp.Value = $DescriptionNode.Value
-		}
-
-		if($Name.StartsWith('_'))
-		{
-			$HiddenProp.Value = 'true'
-		}
-
-		Write-Verbose "Creating report $Name"
-		$warnings = $null
-		$Results = $Proxy.CreateCatalogItem("Report", $Name, $Folder, $true, $RawDefinition, $Properties, [ref]$warnings)
-
-		$Xpath = 'd:Report/d:DataSources/d:DataSource/d:DataSourceReference/..'
-		$DataSources = $Definition.SelectNodes($Xpath, $NsMgr) |
-			ForEach-Object {
-				$DataSourcePath = $DataSourcePaths[$_.DataSourceReference]
-				if (-not $DataSourcePath) {
-					throw "Invalid data source reference '$($_.DataSourceReference)' in $RdlPath"
-				}
-				$Reference = New-Object -TypeName SSRS.ReportingService2010.DataSourceReference
-				$Reference.Reference = $DataSourcePath
-				$DataSource = New-Object -TypeName SSRS.ReportingService2010.DataSource
-				$DataSource.Item = $Reference
-				$DataSource.Name = $_.Name
-				Write-Verbose $DataSource
-			}
-		if ($DataSources) {
-			$Proxy.SetItemDataSources($Folder + '/' + $Name, $DataSources)
-		}
-
-		$Xpath = 'd:Report/d:DataSets/d:DataSet/d:SharedDataSet/d:SharedDataSetReference/../..'
-		$References = $Definition.SelectNodes($Xpath, $NsMgr) |
-			ForEach-Object {
-				$DataSetPath = $DataSetPaths[$_.SharedDataSet.SharedDataSetReference]
-				if ($DataSetPath) {
-					$Reference = New-Object -TypeName SSRS.ReportingService2010.ItemReference
-					$Reference.Reference = $DataSetPath
-					$Reference.Name = $_.Name
-					Write-Verbose $Reference
-				}
-			}
-		if ($References) {
-			$Proxy.SetItemReferences($Folder + '/' + $Name, $References)
-		}
+		New-SSRSReport -Proxy $Proxy -RdlPath $RdlPath
 	}
